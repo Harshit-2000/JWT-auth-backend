e0823df90efb37c33872d4ed6ce82699c5402f1e
Harshit Singh <hrshit2000@gmail.com>
2021-11-18 01:48:23 +0530

    edited settings.py

--- userdetails/settings.py
+++ userdetails/settings.py
@@ -64,7 +64,7 @@ AUTH_USER_MODEL = 'base.User'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
